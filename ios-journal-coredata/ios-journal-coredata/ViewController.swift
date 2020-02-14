a075fdfbc504962ca84c9b166ed3fca74728ea41
Nicolas Rios <nickrios33@yahoo.com>
2020-02-13 16:02:28 -0800

    start of project

---
+++ ios-journal-coredata/ios-journal-coredata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-journal-coredata
+//
+//  Created by Nicolas Rios on 2/13/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
